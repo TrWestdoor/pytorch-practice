db854b75212f6fafa801483d76186a95d626fc1e
TrWestdoor <shiweisongssw@foxmail.com>
2019-12-08 21:55:33 +0800

    update

---
+++ classify.py
@@ -0,0 +1,63 @@
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+
+n_data = torch.ones(100, 2)
+x0 = torch.normal(2*n_data, 1)
+y0 = torch.zeros(100)
+x1 = torch.normal(-2*n_data, 1)
+y1 = torch.ones(100)
+
+x = torch.cat((x0, x1)).type(torch.FloatTensor)
+y = torch.cat((y0, y1)).type(torch.LongTensor)
+
+# print(x.shape, y.shape)
+
+# plt.scatter(x.data.numpy()[:, 0], x.data.numpy()[:, 1], c=y.data.numpy(), s=100, lw=0, cmap='RdYlGn')
+# plt.show()
+
+
+class Net(torch.nn.Module):
+    def __init__(self, n_feature, n_hidden, n_output):
+        super(Net, self).__init__()
+        self.n_hidden = torch.nn.Linear(n_feature, n_hidden)
+        self.out = torch.nn.Linear(n_hidden, n_output)
+
+    def forward(self, x):
+        x = torch.relu(self.n_hidden(x))
+        x = self.out(x)
+        return x
+
+
+net = Net(n_feature=2, n_hidden=10, n_output=2)
+# print(net)
+
+optimizer = torch.optim.SGD(net.parameters(), lr=0.02)
+loss_func = torch.nn.CrossEntropyLoss()
+
+
+for i in range(100):
+    out = net(x)
+    loss = loss_func(out, y)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+
+# train result
+train_result = net(x)
+print(train_result.shape)
+train_predict = torch.max(train_result, 1)[1]
+plt.scatter(x.data.numpy()[:, 0], x.data.numpy()[:, 1], c=train_predict.data.numpy(), s=100, lw=0, cmap='RdYlGn')
+plt.show()
+
+
+# test
+# t_data = torch.zeros(100, 2)
+# test_data = torch.normal(t_data, 5)
+# test_result = net(test_data)
+# prediction = torch.max(test_result, 1)[1]
+#
+# plt.scatter(test_data[:, 0], test_data[:, 1], s=100, c=prediction.data.numpy(), lw=0, cmap='RdYlGn')
+# plt.show()
