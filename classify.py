5ba6f61068148f3712bdf3db487a5917aeccf8b6
TrWestdoor <shiweisongssw@foxmail.com>
2019-12-19 22:50:42 +0800

    update

--- classify.py
+++ classify.py
@@ -8,12 +8,12 @@ x0 = torch.normal(2*n_data, 1)
 y0 = torch.zeros(100)
 x1 = torch.normal(-2*n_data, 1)
 y1 = torch.ones(100)
-x2 = x0
-x2[:, 1] = x1[:, 1]
-y2 = 2 * y1
+# x2 = x0
+# x2[:, 1] = x1[:, 1]
+# y2 = 2 * y1
 
-x = torch.cat((x0, x1, x2)).type(torch.FloatTensor)
-y = torch.cat((y0, y1, y2)).type(torch.LongTensor)
+x = torch.cat((x0, x1)).type(torch.FloatTensor)
+y = torch.cat((y0, y1)).type(torch.LongTensor)
 
 # print(x.shape, y.shape)
 
@@ -27,11 +27,11 @@ class Net(torch.nn.Module):
         self.n_hidden = torch.nn.Linear(n_feature, n_hidden)
         self.out = torch.nn.Linear(n_hidden, n_output)
 
-    def forward(self, x):
-        x = torch.relu(self.n_hidden(x))
-        x = self.out(x)
-        x = torch.nn.functional.softmax(x)
-        return x
+    def forward(self, x_layer):
+        x_layer = torch.relu(self.n_hidden(x_layer))
+        x_layer = self.out(x_layer)
+        x_layer = torch.nn.functional.softmax(x_layer)
+        return x_layer
 
 
 net = Net(n_feature=2, n_hidden=10, n_output=2)
@@ -60,7 +60,7 @@ plt.show()
 
 
 # test
-t_data = torch.zeros(1000, 2)
+t_data = torch.zeros(100, 2)
 test_data = torch.normal(t_data, 5)
 test_result = net(test_data)
 prediction = torch.max(test_result, 1)[1]
