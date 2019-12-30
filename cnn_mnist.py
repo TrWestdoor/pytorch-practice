5af27c567bdb315930e1aa9211812043d1b50c0a
TrWestdoor <shiweisongssw@foxmail.com>
2019-12-30 14:32:37 +0800

    done cnn_mnist practice and adjust some hyper-paramters with good performance :)

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -1,3 +1,92 @@
-from torchvision import datasets
+import torch
+import os
+import torchvision
+import matplotlib.pyplot as plt
+import torch.utils.data as Data
+import torch.nn as nn
 
+
+EPOCH = 1
+BATCH_SIZE = 50
+LR = 0.01
+DOWNLOAD_MNIST = False
+
+
+if not(os.path.exists('./mnist/')) or not os.listdir('./mnist/'):
+    DOWNLOAD_MNIST = True
+
+
+train_data = torchvision.datasets.MNIST(
+    root='./mnist',
+    train=True,
+    transform=torchvision.transforms.ToTensor(),
+    download=DOWNLOAD_MNIST
+)
+
+
+print(train_data.data.size())
+print(train_data.targets.size())
+# plt.imshow(train_data.data[100].numpy(), cmap='gray')
+# plt.title('%i' % train_data.targets[100])
+# plt.show()
+
+
+train_loader = Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
+test_data = torchvision.datasets.MNIST(root='./mnist', train=False)
+test_x = torch.unsqueeze(test_data.data, dim=1).type(torch.FloatTensor)[:2000]/255.
+test_y = test_data.targets[:2000]
+
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=16,
+                kernel_size=5,
+                stride=2,
+                padding=2,
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, 5, 1, 2),
+            nn.ReLU(),
+            # nn.MaxPool2d(2)
+        )
+
+        self.out = nn.Linear(32 * 7 * 7, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(x.size(0), -1)
+        output = self.out(x)
+        return output
+
+
+cnn = CNN()
+# print(cnn)
+
+optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)
+loss_func = nn.CrossEntropyLoss()
+
+
+for epoch in range(EPOCH):
+    for step, (b_x, b_y) in enumerate(train_loader):
+        # print(b_x.shape); break
+        output = cnn(b_x)
+        loss = loss_func(output, b_y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if step % 50 == 0:
+            test_output = cnn(test_x)
+            pred_y = torch.max(test_output, 1)[1].data.numpy()
+            accuracy = float((pred_y == test_y.data.numpy()).astype(int).sum()) / float(test_y.size(0))
+            print('Epoch: ', epoch, '| train loss: %.4f' % loss.data, '| test accuracy: %.2f' % accuracy)
 
