3b6d03214395d09adf5835a0d917123dfaefc5ec
TrWestdoor <shiweisongssw@foxmail.com>
2020-01-07 23:43:22 +0800

    add gpu function

--- cnn_cifar10.py
+++ cnn_cifar10.py
@@ -4,6 +4,8 @@ import torchvision
 import os
 import torch.utils.data as Data
 import torch.nn as nn
+import numpy as np
+import torch.nn.functional as F
 
 
 BATCH_SIZE = 50
@@ -31,42 +33,130 @@ print(train_data.targets.__len__())
 train_loader = Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
 test_loader = torchvision.datasets.CIFAR10(root='./CIFAR10', train=False)
 
-test_x = torch.unsqueeze(torch.from_numpy(test_loader.data), dim=1).type(torch.FloatTensor)
-test_y = test_loader.targets
+test_x = torch.from_numpy(test_loader.data).type(torch.FloatTensor).permute(0, 3, 1, 2)[:200]
+test_y = test_loader.targets[:200]
 
 
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 16, 5, 1, 2),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
+            nn.Conv2d(3, 64, 3, 1, 1),
+            nn.ReLU()
         )
+        # after convolution layer 1, the shape is 64x32x32.
 
         self.conv2 = nn.Sequential(
-            nn.Conv2d(16, 32, 5, 1, 2),
+            nn.Conv2d(64, 128, 3, 1, 1),
+            nn.ReLU()
+        )
+        # after convolution layer 2, the shape is 128x32x32.
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.ReLU()
+        )
+
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        # after convolution layer 4, the shape is 128x16x16.
+
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.ReLU()
+        )
+
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.ReLU()
+        )
+
+        self.conv7 = nn.Sequential(
+            nn.Conv2d(128, 128, 3, 1, 1),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
 
-        self.out = nn.Linear(32 * 8 * 8, 10)
+        # after convolution layer 7, the shape is 128x8x8.
+
+        self.conv8 = nn.Sequential(
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.ReLU()
+        )
+
+        self.conv9 = nn.Sequential(
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        # after convolution layer 9, the shape is 128x4x4.
+
+        self.conv10 = nn.Sequential(
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.ReLU()
+        )
+
+        self.conv11 = nn.Sequential(
+            nn.Conv2d(128, 128, 1, 1, 0),
+            nn.ReLU()
+        )
+        # after convolution layer 11, the shape is 128x4x4.
+
+        self.conv12 = nn.Sequential(
+            nn.Conv2d(128, 128, 1, 1, 0),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        # after convolution layer 12, the shape is 128x2x2.
+
+        self.conv13 = nn.Sequential(
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        # after convolution layer 12, the shape is 128x1x1.
+
+        self.linear = nn.Linear(128 * 1 * 1, 10)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
-        output = self.out(x)
-        return output
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
+        x = self.conv7(x)
+        x = self.conv8(x)
+        x = self.conv9(x)
+        x = self.conv10(x)
+        x = self.conv11(x)
+        x = self.conv12(x)
+        x = self.conv13(x)
+        x = x.view(x.size(0), -1)
 
+        linear_result = self.linear(x)
+        output_layer = F.softmax(linear_result)
+        return output_layer
 
+gpus = [0]
+cuda_gpu = torch.cuda.is_available()
 cnn = CNN()
-print(cnn)
+# print(cnn)
+
+if cuda_gpu:
+    cnn = torch.nn.DataParallel(cnn, device_ids=gpus).cuda()
 
 
 optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)
 loss_func = nn.CrossEntropyLoss()
 for epoch in range(EPOCH):
     for step, (b_x, b_y) in enumerate(train_loader):
+        if cuda_gpu:
+            b_x = b_x.cuda()
+            b_y = b_y.cuda()
         output = cnn(b_x)
         loss = loss_func(output, b_y)
         optimizer.zero_grad()
@@ -75,6 +165,10 @@ for epoch in range(EPOCH):
 
         if step % 50 == 0:
             test_out = cnn(test_x)
-            pred_y = torch.max(test_out, 1)[1].data.numpy()
-            accuracy = float((pred_y == test_y.data.numpy()).astype(int).sum()) / float(test_y.size(0))
-
+            pred_y = torch.max(test_out, 1)[1].data
+            if cuda_gpu:
+                pred_y = pred_y.cpu().numpy()
+            else:
+                pred_y = pred_y.numpy()
+            accuracy = float((pred_y == np.array(test_y)).astype(int).sum()) / float(len(test_y))
+            print('Epoch: ', epoch, '| train loss: %.4f' % loss.data, '| test accuracy: %.2f' % accuracy)
--- cnn_mnist.py
+++ cnn_mnist.py
@@ -4,6 +4,7 @@ import torchvision
 import matplotlib.pyplot as plt
 import torch.utils.data as Data
 import torch.nn as nn
+import time
 
 
 EPOCH = 1
@@ -33,8 +34,8 @@ print(train_data.targets.size())
 
 train_loader = Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
 test_data = torchvision.datasets.MNIST(root='./mnist', train=False)
-test_x = torch.unsqueeze(test_data.data, dim=1).type(torch.FloatTensor)[:2000]/255.
-test_y = test_data.targets[:2000]
+test_x = torch.unsqueeze(test_data.data, dim=1).type(torch.FloatTensor)/255.
+test_y = test_data.targets
 
 
 class CNN(nn.Module):
@@ -67,17 +68,27 @@ class CNN(nn.Module):
         output = self.out(x)
         return output
 
-
+gpus = [0]
+cuda_gpu = torch.cuda.is_available()
 cnn = CNN()
 # print(cnn)
+
+if cuda_gpu:
+    cnn = torch.nn.DataParallel(cnn, device_ids=gpus).cuda()
 
 optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)
 loss_func = nn.CrossEntropyLoss()
 
 
+time_start = time.time()
 for epoch in range(EPOCH):
     for step, (b_x, b_y) in enumerate(train_loader):
         # print(b_x.shape); break
+
+        if cuda_gpu:
+            b_x = b_x.cuda()
+            b_y = b_y.cuda()
+
         output = cnn(b_x)
         loss = loss_func(output, b_y)
         optimizer.zero_grad()
@@ -86,7 +97,15 @@ for epoch in range(EPOCH):
 
         if step % 50 == 0:
             test_output = cnn(test_x)
-            pred_y = torch.max(test_output, 1)[1].data.numpy()
+            pred_y = torch.max(test_output, 1)[1].data
+
+            if cuda_gpu:
+                pred_y = pred_y.cpu().numpy()
+            else:
+                pred_y = pred_y.numpy()
+
             accuracy = float((pred_y == test_y.data.numpy()).astype(int).sum()) / float(test_y.size(0))
             print('Epoch: ', epoch, '| train loss: %.4f' % loss.data, '| test accuracy: %.2f' % accuracy)
 
+time_end = time.time()
+print("all time spent: ", time_end-time_start)
